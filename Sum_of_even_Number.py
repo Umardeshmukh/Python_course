cb2af0d18eb906505938cce3e681861ab1387ea8
Umardeshmukh <118596403+Umardeshmukh@users.noreply.github.com>
2024-02-04 15:38:39 +0530

    Create Sum_of_even_Number.py

---
+++ Sum_of_even_Number.py
@@ -0,0 +1,12 @@
+target = int(input()) # Enter a number between 0 and 1000
+# 🚨 Do not change the code above ☝️
+
+
+
+even_total = 0
+for i in range(2, target +1, 2):
+ even_total += i
+print(even_total)  
+  
+  
+  
